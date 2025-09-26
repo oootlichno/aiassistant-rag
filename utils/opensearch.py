52b7dc8a862b89bee0f489a7d7bb3a0e94475d9b
Kseniia Kondrat <oootlichno@gmail.com>
2025-09-26 15:56:30 -0500

    WIP

--- utils/opensearch.py
+++ utils/opensearch.py
@@ -1,34 +1,42 @@
 import boto3
-from opensearchpy import OpenSearch, RequestsHttpConnection
 from requests_aws4auth import AWS4Auth
+from opensearchpy import OpenSearch, RequestsHttpConnection
 
-region = "us-east-2"
+region = "us-east-2"  
 service = "es"
 INDEX_NAME = "documents"
 
-def get_opensearch_client():
-    session = boto3.Session(region_name=region)
-    credentials = session.get_credentials()
-    aws_auth = AWS4Auth(
-        credentials.access_key,
-        credentials.secret_key,
-        credentials.token,
-        region,
-        service
-    )
+# Get credentials from EC2 instance role
+session = boto3.Session()
+credentials = session.get_credentials().get_frozen_credentials()
 
-    OPENSEARCH_ENDPOINT = "search-aiassistant-vectors-dev-d4jiukbmp2erksrnnwtcwvdlcm.us-east-2.es.amazonaws.com"
+aws_auth = AWS4Auth(
+    credentials.access_key,
+    credentials.secret_key,
+    credentials.token,
+    region,
+    service
+)
 
-    return OpenSearch(
-        hosts=[{"host": OPENSEARCH_ENDPOINT, "port": 443}],
-        http_auth=aws_auth,
-        use_ssl=True,
-        verify_certs=True,
-        connection_class=RequestsHttpConnection
-    )
+OPENSEARCH_ENDPOINT = "search-aiassistant-vectors-dev-d4jiukbmp2erksrnnwtcwvdlcm.us-east-2.es.amazonaws.com"
 
-def search_index(query, top_k=2):
-    client = get_opensearch_client()
-    body = {"query": {"match": {"content": query}}}
+client = OpenSearch(
+    hosts=[{"host": OPENSEARCH_ENDPOINT, "port": 443}],
+    http_auth=aws_auth,
+    use_ssl=True,
+    verify_certs=True,
+    connection_class=RequestsHttpConnection
+)
+
+
+def search_index(query, top_k=3):
+    body = {
+        "query": {
+            "multi_match": {
+                "query": query,
+                "fields": ["title", "content"]
+            }
+        }
+    }
     res = client.search(index=INDEX_NAME, body=body, size=top_k)
-    return [hit["_source"]["content"] for hit in res["hits"]["hits"]]
+    return res["hits"]["hits"]
